022fd1b546d84346a1140ae37fd3d6138defa358
LiuKinJohn <201305227+LiuKinJohn@users.noreply.github.com>
2026-04-16 13:53:19 +0800

    Fix arm variant and add preflight checks for build stability

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,4 +2,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_pocketalks.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_pocketalks-eng
+    omni_pocketalks-eng \
+    omni_pocketalks-userdebug
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,8 @@
 DEVICE_PATH := device/pocketalk/pocketalks
 
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+# twrp-11 build system expects armv8-a for MT6739-class Cortex-A53 targets.
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
