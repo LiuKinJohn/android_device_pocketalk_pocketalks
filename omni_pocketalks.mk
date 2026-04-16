cd45cb4c046257243949c4f40af1d3043fd481ba
LiuKinJohn <201305227+LiuKinJohn@users.noreply.github.com>
2026-04-16 13:39:53 +0800

    Fix product include compatibility and add lunch fallback

--- omni_pocketalks.mk
+++ omni_pocketalks.mk
@@ -1,4 +1,20 @@
+# Different TWRP/AOSP bases place product core files in different paths.
+ifneq (,$(wildcard $(SRC_TARGET_DIR)/product/core_32_bit.mk))
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_32_bit.mk)
+else ifneq (,$(wildcard build/make/target/product/core_32_bit.mk))
+$(call inherit-product, build/make/target/product/core_32_bit.mk)
+else ifneq (,$(wildcard build/target/product/core_32_bit.mk))
+$(call inherit-product, build/target/product/core_32_bit.mk)
+else ifneq (,$(wildcard $(SRC_TARGET_DIR)/product/core_minimal.mk))
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+else ifneq (,$(wildcard build/make/target/product/core_minimal.mk))
+$(call inherit-product, build/make/target/product/core_minimal.mk)
+else ifneq (,$(wildcard build/target/product/core_minimal.mk))
+$(call inherit-product, build/target/product/core_minimal.mk)
+else
+$(error Could not find core product makefile in this source tree)
+endif
+
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := pocketalks
